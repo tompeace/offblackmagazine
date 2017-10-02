e342f763a337ba301546dab138567fe7f42217b4
Tom <wt.peace@gmail.com>
2017-10-03 00:36:30 +0100

    added back and next links to detail page

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -1,5 +1,5 @@
-
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Carousel from '../partials/carousel.jsx'
 import Menu from '../partials/menu.jsx'
 
@@ -8,50 +8,52 @@ class Home extends React.Component {
     render() {
         return (
             <section className='relative height-100 overflow-hidden'>
-                <div className='table'>
-                    <div className='tr'>
-                        <Carousel
-                            imgClassName='center-carousel-img'
-                            className='td td-1'
-                            borderClass='border-top border-bottom border-left border-right border-black'
-                            interval={2000}
-                            items={[
-                                {src: './build/images/OB_left-01.jpg'},
-                                {src: './build/images/OB_left-02.jpg'},
-                                {src: './build/images/OB_left-03-04.jpg'},
-                                {src: './build/images/OB_left-03-04.jpg'},
-                                {src: './build/images/OB_left-05-06.gif'},
-                                {src: './build/images/OB_left-05-06.gif'},
-                                {src: './build/images/OB_left-07.jpg'},
-                                {src: './build/images/OB_left-08-09.jpg'},
-                                {src: './build/images/OB_left-08-09.jpg'},
-                                {src: './build/images/OB_left-10.jpg'},
-                                {src: './build/images/OB_left-11.jpg'},
-                                {src: './build/images/OB_left-12.jpg'}
-                            ]}
-                        />
-                        <Carousel
-                            imgClassName='center-carousel-img'
-                            className='td td-2'
-                            borderClass='border-top border-bottom border-right border-black'
-                            interval={2000}
-                            items={[
-                                {src: './build/images/OB_right-01.jpg'},
-                                {src: './build/images/OB_right-02-03.jpg'},
-                                {src: './build/images/OB_right-02-03.jpg'},
-                                {src: './build/images/OB_right-04-05.jpg'},
-                                {src: './build/images/OB_right-04-05.jpg'},
-                                {src: './build/images/OB_right-06-07.jpg'},
-                                {src: './build/images/OB_right-06-07.jpg'},
-                                {src: './build/images/OB_right-08.jpg'},
-                                {src: './build/images/OB_right-09-10.gif'},
-                                {src: './build/images/OB_right-09-10.gif'},
-                                {src: './build/images/OB_right-11-12.jpg'},
-                                {src: './build/images/OB_right-11-12.jpg'}
-                            ]}
-                        />
+                <Link to='/stories'>
+                    <div className='table'>
+                        <div className='tr'>
+                            <Carousel
+                                imgClassName='center-carousel-img'
+                                className='td td-1'
+                                borderClass='border-top border-bottom border-left border-right border-black'
+                                interval={2000}
+                                items={[
+                                    {src: './build/images/OB_left-01.jpg'},
+                                    {src: './build/images/OB_left-02.jpg'},
+                                    {src: './build/images/OB_left-03-04.jpg'},
+                                    {src: './build/images/OB_left-03-04.jpg'},
+                                    {src: './build/images/OB_left-05-06.gif'},
+                                    {src: './build/images/OB_left-05-06.gif'},
+                                    {src: './build/images/OB_left-07.jpg'},
+                                    {src: './build/images/OB_left-08-09.jpg'},
+                                    {src: './build/images/OB_left-08-09.jpg'},
+                                    {src: './build/images/OB_left-10.jpg'},
+                                    {src: './build/images/OB_left-11.jpg'},
+                                    {src: './build/images/OB_left-12.jpg'}
+                                ]}
+                            />
+                            <Carousel
+                                imgClassName='center-carousel-img'
+                                className='td td-2'
+                                borderClass='border-top border-bottom border-right border-black'
+                                interval={2000}
+                                items={[
+                                    {src: './build/images/OB_right-01.jpg'},
+                                    {src: './build/images/OB_right-02-03.jpg'},
+                                    {src: './build/images/OB_right-02-03.jpg'},
+                                    {src: './build/images/OB_right-04-05.jpg'},
+                                    {src: './build/images/OB_right-04-05.jpg'},
+                                    {src: './build/images/OB_right-06-07.jpg'},
+                                    {src: './build/images/OB_right-06-07.jpg'},
+                                    {src: './build/images/OB_right-08.jpg'},
+                                    {src: './build/images/OB_right-09-10.gif'},
+                                    {src: './build/images/OB_right-09-10.gif'},
+                                    {src: './build/images/OB_right-11-12.jpg'},
+                                    {src: './build/images/OB_right-11-12.jpg'}
+                                ]}
+                            />
+                        </div>
                     </div>
-                </div>
+                </Link>
             </section>
         )
     }
--- src/components/partials/header.jsx
+++ src/components/partials/header.jsx
@@ -6,9 +6,14 @@ class Header extends React.Component {
     constructor() {
         super()
         this.handleToggle = this.handleToggle.bind(this)
+        this.handleBlur = this.handleBlur.bind(this)
         this.state = {
             menuActive: false
         }
+    }
+
+    handleBlur() {
+        console.log('blurrrr');
     }
 
     handleToggle() {
@@ -19,20 +24,22 @@ class Header extends React.Component {
 
     render() {
         return (
-            <header className='z1 relative clearfix'>
+            <header onBlur={this.handleBlur} className='z1 relative clearfix'>
                 <div style={{height: '67px'}} className="col-12"></div>
                 <div>
                     <div className={`z1 fixed left-0 right-0 top-0 ${this.state.menuActive ? 'bottom-0' :''}`}>
-                        <div className="bg-white mr3 ml3 mt3 p2 border border-black line-height-2">
+                        <div className="bg-white mr3 ml3 mt3 p2 border border-black line-height-2 cursor-pointer">
                             Anthology Issue A/W 2017
                             <span
                                 onClick={this.handleToggle}
                                 className='right'>
-                                Menu
+                                {this.state.menuActive ? 'Close' :'Menu'}
                             </span>
                         </div>
                         {this.state.menuActive && (
-                            <Menu onHandleToggle={this.handleToggle} />
+                            <div className='pl3'>
+                                <Menu onHandleToggle={this.handleToggle} />
+                            </div>
                         )}
                     </div>
                 </div>
